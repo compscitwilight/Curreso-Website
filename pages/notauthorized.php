aaf072b6f70b89fa3a188adb2bd8edd58fffb5b6
Blog <57299745+shinyish@users.noreply.github.com>
2022-01-03 06:37:59 -0800

    Create notauthorized.php

---
+++ pages/notauthorized.php
@@ -0,0 +1,11 @@
+<?php
+include "../includes/header.php"
+?>
+
+<div class="not-authorized">
+    <p>You do not have permission to view this page.</p>
+</div>
+
+<?php
+include "../includes/footer.php"
+?>
