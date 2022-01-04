c098c4cef834166cbd65f96eaf5349569ece19fb
Blog <57299745+shinyish@users.noreply.github.com>
2022-01-04 06:16:59 -0800

    Create postaboutme.inc.php

---
+++ includes/inc/postaboutme.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    $aboutMe = $_POST["about-me"];
+
+    require_once "dbh.inc.php";
+    require_once "functions.inc.php";
+
+    postAboutMe($conn, $aboutMe);
+}
