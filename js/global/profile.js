ee50c2ece962e19f9174c951e07bae6082232af2
Blog <57299745+shinyish@users.noreply.github.com>
2022-01-04 05:19:48 -0800

    Update profile.js

--- js/global/profile.js
+++ js/global/profile.js
@@ -1,1 +1,16 @@
+const aboutMeEdit = document.getElementById("about-me-edit");
+const editProfileButton = document.getElementById("edit-btn");
 
+let editingProfile = false;
+
+editProfileButton.addEventListener('click', () => {
+    if (!editingProfile) {
+        editingProfile = true;
+
+        aboutMeEdit.style.visibility = "visbile";
+    } else {
+        editingProfile = false;
+
+        aboutMeEdit.style.visibility = "hidden";
+    }
+})
