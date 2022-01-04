00e4a1c402323c098c2e9b9da2908d6489169952
Blog <57299745+shinyish@users.noreply.github.com>
2022-01-04 13:25:10 -0800

    Update home.php

--- pages/shop/home.php
+++ pages/shop/home.php
@@ -1,17 +1,22 @@
 <?php
 include "../../includes/header.php";
 include "./includes/search.php";
+
+include "../../includes/inc/dbh.inc.php";
 ?>
 
 <div class="shop-container">
     <ul class="shop-container-content">
-        <li class="shop-list-item">
-            <a href="/pages/shop/items/item.php?id=">test</a>
-            <?php
-            echo '<p>BX$' . abbreviateNumber(100000) . '</p>';
-            ?>
-            <img src="/images/coin.png">
-        </li>
+        <?php
+        $sql = "SELECT * FROM `items`";
+        $qry = mysqli_query($conn, $sql);
+
+        while ($result = mysqli_fetch_array($qry)) {
+            if ($result["onSale"] == 1) {
+                echo '<li class="shop-list-item"><a href="/pages/shop/items/item.php?id=' . $result["itemsId"] . '">' . $result["itemsName"] . '</a><p>BX$' . abbreviateNumber($result["itemsPrice"]) . '</p><img src="/images/coin.png"</li>';
+            }
+        }
+        ?>
     </ul>
 </div>
 
