64b024b0f93b1e766a28cf04d3bc5e1786e40904
Blog <57299745+shinyish@users.noreply.github.com>
2022-01-04 12:58:30 -0800

    Update home.php

--- pages/shop/home.php
+++ pages/shop/home.php
@@ -1,12 +1,15 @@
 <?php
 include "../../includes/header.php";
+include "../../includes/inc/functions.inc.php";
 include "./includes/search.php";
 ?>
 
 <div class="shop-container">
     <ul class="shop-container-content">
         <li class="shop-list-item">
-            <img>
+            <a href="/pages/shop/items/item.php?id=">test</a>
+            <p></p>
+            <img src="/images/coin.png">
         </li>
     </ul>
 </div>
