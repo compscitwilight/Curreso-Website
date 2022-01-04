20a84b64517226f8065bfa8ea95c52a31ddc1d31
Blog <57299745+shinyish@users.noreply.github.com>
2022-01-04 11:19:23 -0800

    Update inventory.php

--- pages/users/inventory.php
+++ pages/users/inventory.php
@@ -1,1 +1,15 @@
+<?php
+require_once "../../includes/inc/dbh.inc.php";
+include "../../includes/header.php";
 
+if (isset($_GET["id"])) {
+    if ($_GET["id"] == "" || !$_GET["id"]) {
+        header("LOCATION: /pages/users/inventory.php?id=" . $_SESSION["userid"]);
+    }
+}
+?>
+
+<?php
+include "../../includes/footer.php";
+echo '<script src="/js/global/profile.js"></script>';
+?>
