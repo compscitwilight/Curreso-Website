81ba69d02d23fb2133cbd675bf6bb7381b5e0175
Blog <57299745+shinyish@users.noreply.github.com>
2022-01-04 12:28:52 -0800

    Update search.php

--- pages/shop/includes/search.php
+++ pages/shop/includes/search.php
@@ -1,5 +1,6 @@
 <div class="shop-search">
     <form action="" method="POST">
-
+        <input name="search" type="search" placeholder="Search" required>
+        <button class="btn-medium" name="submit" type="submit">Search</button>
     </form>
 </div>
